ce47313bf1bcfe5bd2e1e0e3cc06d4cc545250cf
Joao Silva <121130958+JMASilva28@users.noreply.github.com>
2023-03-27 22:44:24 +0100

    Add files via upload
    Add project files.

---
+++ D04_EF6_Migrations_DAL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace D04_EF6_Migrations_DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<D04_EF6_Migrations_DAL.BookDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(D04_EF6_Migrations_DAL.BookDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ D04_EF6_Migrations_DAL/Model/BookDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace D04_EF6_Migrations_DAL
+{
+
+    public class BookDBContext : DbContext
+    {
+
+        // O nome da connectionstring a ser usada neste contexto
+        public BookDBContext() : base("BooksDB_Migrations")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Publisher> Publisher { get; set; }
+        public DbSet<Book> Book { get; set; }
+
+    }
+
+}
