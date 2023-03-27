b163567206a3047ea71b3c4c2e1afed72e66a22f
Joao Silva <121130958+JMASilva28@users.noreply.github.com>
2023-03-27 22:42:53 +0100

    Add files via upload
    Add project files.

---
+++ D04_EF6_Migrations_Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D04_EF6_Migrations_Client
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            BooksDBClient.CreatePublisher();
+            BooksDBClient.CreateBook();
+
+            Console.Write("Data entered successfully.");
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
